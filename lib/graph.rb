55b0fc8b1a334e9dcd0c6fe792bdb608951ad2ee
diebels727 <diebelsalternative@hotmail.com>
2014-11-18 09:05:58 -0500

    Add graph representation

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,7 +1,40 @@
 class Graph
-  attr_reader :vertices,:edges
+  class AdjacencyList
+    def initialize()
+      @adjacency_list = {}
+    end
+    def <<(edge)
+      tail = edge.tail
+      head = edge.head
+      tail_id = tail.id
+      head_id = head.id
+      @adjacency_list[tail_id] = (@adjacency_list[tail_id]||[]) << edge
+      @adjacency_list[head_id] = @adjacency_list[head_id] || []
+    end
+    def vertices
+      @adjacency_list.keys
+    end
+  end
+
   def initialize()
-    @vertices = []
+    #I chose to represent a graph as an adjacency list; O(m+n) in memory
+    #  but, there may be other ways and reasons to represent a graph differently.
+    #  so let's not couple ourselves too tightly to and adj list
+    @graph_representation = AdjacencyList.new
     @edges = []
   end
+
+  def vertices
+    @graph_representation.vertices
+  end
+
+  def edges
+    @edges
+  end
+
+  def add_edge(edge)
+    @graph_representation << edge
+    @edges << edge
+  end
+
 end
