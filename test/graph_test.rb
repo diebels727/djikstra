7b8e68f12ce7dda6bdd9ee5ea6187fe2654bdf1b
diebels727 <diebelsalternative@hotmail.com>
2014-11-18 09:13:09 -0500

    Add some graph tests.

--- test/graph_test.rb
+++ test/graph_test.rb
@@ -1,5 +1,21 @@
 require "test_helper"
 
+class MockEdge
+  attr_accessor :head,:tail,:weight
+  def initialize(head,tail,weight)
+    @head = MockVertex.new head
+    @tail = MockVertex.new tail
+    @weight = weight
+  end
+end
+
+class MockVertex
+  attr_accessor :id
+  def initialize(id)
+    @id = id
+  end
+end
+
 describe Graph do
   subject {Graph.new}
   it "creates an empty graph" do
@@ -14,5 +30,11 @@ describe Graph do
     subject.edges.must_be_empty
   end
 
+  it "adds an edge" do
+    edge = MockEdge.new("A","B",1)
+    subject.add_edge(edge)
+    subject.edges.size.must_be :==,1
+    subject.vertices.size.must_be :==,2
+  end
 
 end
